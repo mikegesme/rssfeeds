6b8392d4b7bfdef385a8b45ffa09cd6fbf6e1a04
Mike Gesme <mikegesme@Mikes-MacBook-Pro.local>
2018-04-14 12:32:39 -0500

    Update feed parsing script.

--- pullfeed.js
+++ pullfeed.js
@@ -2,10 +2,29 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs      = require('fs');
 
+var regExp = /When.+?2018/;
+
 request('http://www.cannonfalls.org/event/feed/', function (error, response, html) {
   if (!error && response.statusCode == 200) {
-    // console.log(html);
-    fs.writeFile("events.html", html, function(err) {
+    $ = cheerio.load(html, { xmlMode: true });
+
+    $('item').each(function(i, item) {
+        innerHTML = $(item).text();
+        innerHTML = innerHTML.replace(/\s\s+/g, ' ');
+        innerHTML = innerHTML.replace('</strong>', '');
+        var matches = regExp.exec(innerHTML);
+        if(matches[0]) {
+            var date = new Date(matches[0]);
+            var today = new Date();
+            var twomonths = new Date();
+            twomonths.setDate(today.getDate() + 60);
+            if (!(date >= today && date <= twomonths)) {
+                $(item).remove();
+            }
+        }
+    });
+
+    fs.writeFile("events.html", $.html(), function(err) {
         if(err) {
             return console.log(err);
         }
